0fece89ce12d9e6b6c963ef7ae9b1c9ecdb9ecf6
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-05-24 00:26:01 +0300

    cleanup done, dependencies installed, react part works fine. Hardhat part will be tested before next push

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+
+const hre = require("hardhat");
+
+async function main() {
+  const Orange = await hre.ethers.getContractFactory("Orange");
+  const orange = await Orange.deploy();
+
+  await orange.deployed();
+
+  console.log(`${orange} deployed to ${orange.address}`);
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
