4ae79918225d2fff2af6fa886a641c0294d77d6f
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-05-24 22:33:00 +0300

    test deployment successful to fantom but to bnb it failed, also I couldnt get test bnb tokens, shit bnb

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,16 @@
 
+
 const hre = require("hardhat");
 
 async function main() {
+
+
   const Orange = await hre.ethers.getContractFactory("Orange");
   const orange = await Orange.deploy();
 
   await orange.deployed();
 
-  console.log(`${orange} deployed to ${orange.address}`);
+  console.log(`orange  deployed to ${orange.address}`);
 }
 
 
@@ -15,3 +18,6 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+
