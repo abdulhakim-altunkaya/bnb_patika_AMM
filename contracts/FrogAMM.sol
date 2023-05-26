2c745d5caae5710c41151e6cd97de6f31d87c18e
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-05-26 22:06:13 +0300

    removal liquidity functions finished

--- contracts/FrogAMM.sol
+++ contracts/FrogAMM.sol
@@ -40,4 +40,68 @@ contract FrogAMM {
         reserveB += amountB;
     }
 
+    function removeLiquidityTokenA(uint amountA) external {
+        require(amountA > 0, "removal amount must be bigger than 0");
+
+        //we need to withdraw a proportional amount from tokenB also to keep the balance of the pool
+        //To do so, we use a basic mathematical proportion.
+        uint amountB = (amountA * reserveB) / reserveA;
+
+        //decrease the reserves
+        reserveA -= amountA;
+        reserveB -= amountB;
+
+        //transfer tokens back to msg.sender
+        IERC20(tokenA).transfer(msg.sender, amountA);
+        IERC20(tokenB).transfer(msg.sender, amountB);
+    }
+
+    function removeLiquidityTokenB(uint amountB) external {
+        require(amountB > 0, "removal amount must be bigger than 0");
+
+        //calculate corresponding amount as above
+        uint amountA = (amountB * reserveA) / reserveB;
+
+        //decrease the reserves
+        reserveA -= amountA;
+        reserveB -= amountB;
+
+        //transfer tokens to the msg.sender
+        IERC20(tokenA).transfer(msg.sender, amountA);
+        IERC20(tokenB).transfer(msg.sender, amountB);
+    }
+
+
+}
+/*
+You can add 10**18 to make calculation easier */
+
+contract AMM {
+
+
+
+    function swap(uint256 amountIn, uint256 amountOutMin) external {
+        require(amountIn > 0, "Amount must be greater than zero");
+
+        // Calculate the output amount using the constant product formula
+        uint256 amountOut = (amountIn * reserveB) / reserveA;
+
+        // Ensure the calculated amountOut is greater than or equal to the minimum specified
+        require(amountOut >= amountOutMin, "Insufficient output amount");
+
+        // Transfer tokenIn from the sender to the contract
+        IERC20(tokenA).transferFrom(msg.sender, address(this), amountIn);
+
+        // Transfer tokenOut from the contract to the sender
+        IERC20(tokenB).transfer(msg.sender, amountOut);
+
+        // Update the reserves
+        reserveA += amountIn;
+        reserveB -= amountOut;
+    }
+}
+
+interface IERC20 {
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function transfer(address recipient, uint256 amount) external returns (bool);
 }
