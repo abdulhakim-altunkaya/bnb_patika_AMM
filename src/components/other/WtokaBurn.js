4c80c5344b68147369dbe615bf617f617b2c3b27
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-06-06 10:19:49 +0300

    tokena components created including style

--- src/components/other/WtokaBurn.js
+++ src/components/other/WtokaBurn.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 
 function WtokaBurn() {
+
+  let [amount, setAmount] = useState("");
+
+  const burnToken = async () => {
+    console.log(amount);
+  }
+
   return (
-    <div>WtokaBurn</div>
+  <div>
+      <button onClick={burnToken} className='button4'>Burn TokenA</button> {amount}
+      <input type="number" placeholder='enter amount' className='inputFields'
+        value={amount} onChange={ e => setAmount(e.target.value)}/>
+    </div>
   )
 }
 
--- src/components/other/WtokaMint.js
+++ src/components/other/WtokaMint.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function WtokaMint() {
+
+  let [amount, setAmount] = useState("");
+
+  const mintToken = async () => {
+    console.log(amount);
+  }
   return (
-    <div>WtokaMint</div>
+    <div>
+      <button onClick={mintToken} className='button4'>Mint TokenA</button> {amount}
+      <input type="number" placeholder='Mint between 1-499' className='inputFields'
+        value={amount} onChange={ e => setAmount(e.target.value)}/>
+    </div>
   )
 }
 
